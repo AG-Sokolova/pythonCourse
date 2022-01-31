641a8ac5df835ea3eeb00d4b10a42a5261a887fc
qaSokolovaAG <nast1ageorg@gmail.com>
2022-01-31 07:34:15 +0400

    edit task 1-2 and add task 4

--- HW_4.1/task_1.py
+++ HW_4.1/task_1.py
@@ -1,9 +1,12 @@
 # https://cash.rbc.ru/converter.html?from=RUR&to=CNY&sum=5000&date=&rate=cbrf
+
 print('\033[44m' + '\033[30m' + 'Конвертер российского рубля к доллару США' + '\033[0m')
 
 item_usd = 0.0129
 
-amountMoney = int(input('\033[1m' + 'Введите количество денег: ' + '\033[m'))
-converter = item_usd * amountMoney
+amount_money = input('\033[1m' + 'Введите количество денег: ' + '\033[m')
+amount_money = int(amount_money)
 
-print('Конвертированная сумма', amountMoney, 'рублей =', converter, 'долларов')
+converter = item_usd * amount_money
+
+print(f'Конвертированная сумма {amount_money} рублей = {converter} долларов')
--- HW_4.1/task_2.py
+++ HW_4.1/task_2.py
@@ -1,17 +1,26 @@
 # https://cash.rbc.ru/converter.html?from=RUR&to=CNY&sum=5000&date=&rate=cbrf
 print('\033[44m' + '\033[30m' + 'Конвертер российского рубля' + '\033[0m')
 
-item_usd = 0.0129  # => Доллар США
-item_eur = 0.0114  # => ЕВРО
-item_chf = 0.0118  # => Швейцарский франк
-item_gbr = 0.0095  # => Фунт стерлингов
-item_cny = 0.0818  # => Китайский юань
+dict_cur = {
+    'item_usd': 'долларов',
+    'item_eur': 'евро',
+    'item_chf': 'франков',
+    'item_gbr': 'стерлингов',
+    'item_cny': 'юань'
+}
+dict_items = {
+    'item_usd': 0.0129,  # => Доллар США
+    'item_eur': 0.0114,  # => ЕВРО
+    'item_chf': 0.0118,  # => Швейцарский франк
+    'item_gbr': 0.0095,  # => Фунт стерлингов
+    'item_cny': 0.0818  # => Китайский юань
+}
 
-list_item = [item_usd, item_eur, item_chf, item_gbr, item_cny]
-list_currency = ['долларов', 'евро', 'франков', 'стерлингов', 'юань']
+amount_money = input('\033[1m' + 'Введите количество денег: ' + '\033[0m')  # => российский рубль
+amount_money = int(amount_money)
 
-amountMoney = int(input('\033[1m' + 'Введите количество денег: ' + '\033[0m'))  # => российский рубль
-
-for i in range(len(list_item)):
-    converter = round(list_item[i] * amountMoney, 2)
-    print('Конвертированная сумма', amountMoney, 'рублей =', converter, list_currency[i])
+for k_item, v_item in dict_items.items():
+    for k_cur, v_cur in dict_cur.items():
+        if k_item == k_cur:
+            converter = round(amount_money * v_item, 2)
+            print(f'Конвертированная сумма {amount_money} рублей = {converter} {v_cur}')
