66375999449a502e79ce764be689bacdf4db84ab
qaSokolovaAG <nast1ageorg@gmail.com>
2022-02-03 21:22:02 +0400

    edit HW_5.1

--- HW_5_1/func.py
+++ HW_5_1/func.py
@@ -18,10 +18,47 @@ def plit_list(array, size):
         new_array.append(size_items)
         copy_array = copy_array[size:]
     new_array.append(copy_array)
-    
+
     return new_array
 
 
 # 10
 def lists_even_odd(array):
-    print()
+    new_arr_even = []
+    new_arr_odd = []
+
+    for items in array:
+        if items % 2 == 0:
+            new_arr_even.append(items)
+        elif items % 2 != 0:
+            new_arr_odd.append(items)
+    return new_arr_even, new_arr_odd
+
+
+# 13
+def list_100(array):
+    list_less_100 = []  # => меньше 100
+    list_more_100 = []  # => больше или равно 100
+
+    count_less = 0
+    count_more = 0
+
+    for i in array:
+        for j in i:
+            if j >= 100:
+                list_more_100.append(j)
+                count_more += j
+            else:
+                list_less_100.append(j)
+                count_less += j
+
+    if len(list_more_100) == 0:
+        list_more_100.append('No lists')
+        count_more = 'No lists'
+    if len(list_less_100) == 0:
+        list_less_100.append('No lists')
+        count_less = 'No lists'
+
+    return list_less_100, list_more_100, count_less, count_more
+
+# 16
