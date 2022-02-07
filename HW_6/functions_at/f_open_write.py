7d88112f4bb29d2c52b9643dfeb53792cab62696
qaSokolovaAG <nast1ageorg@gmail.com>
2022-02-07 18:48:17 +0400

    add HW6

--- HW_6/functions_at/f_open_write.py
+++ HW_6/functions_at/f_open_write.py
@@ -1,5 +1,26 @@
 import csv
-from additionally import gen_email
+import random
+
+
+# генерирует рандомно имя email
+def rnd_name_mail(length):
+    letters = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    rand_string = ''.join(random.choice(letters) for i in range(length))
+    return rand_string
+
+
+# из строки "Name Lastname" получает email "n.lastname@gmail.com
+def gen_email(user):
+    name, last_name = "", ""
+    full_name = user.split()
+
+    for item, value in enumerate(full_name):
+        if item == 0:
+            name = value[0]
+        elif item == 1:
+            last_name = value
+    email_name = f'{name}.{last_name}@gmail.com'
+    return email_name.lower()
 
 
 # создает пустой файл
@@ -46,11 +67,6 @@ def add_names_csv(file_title, num_lines, list_names):
 #             write.writerow(mail)
 #             i += 1
 #     email_csv.close()
-
-
-# def add_emails_csv(file_title, list_name):
-#     with open(file_title, 'w', newline=''):
-#         col = ['email']
 
 
 def add_emails_csv(file_title, num_lines, list_names, i=0):
