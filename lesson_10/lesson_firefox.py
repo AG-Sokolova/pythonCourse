21fa5f83fed9b834a749fb4d89d475b1ad49ef2f
Sokolova_AG <vskwolf1@gmail.com>
2022-03-02 15:05:22 +0400

    edit file lesson_firefox.py

--- lesson_10/lesson_firefox.py
+++ lesson_10/lesson_firefox.py
@@ -1,24 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.by import By
 import time
 
 driver = webdriver.Firefox(service=Service(executable_path='./selenium/geckodriver'))
 driver.get("http://itcareer.pythonanywhere.com")
 
-# web_form = driver.find_element_by_class_name('form_group')
+# web_form = driver.find_element(By.CLASS_NAME, 'form-group')
 # print(web_form.get_attribute('innerHTML'))
 
-name_field = driver.find_element_by_id('name')
-name_field.send_keys('Inna')
+def field_entry(element, data):
+    filed = driver.find_element(By.ID, element).send_keys(data)
+    return filed
 
-surname_field = driver.find_element_by_id('surname')
-surname_field.send_keys('Didnt')
 
-email_field = driver.find_element_by_id('email')
-email_field.send_keys('inn_did@gmail.com')
+field_entry('name', 'Inna')
+field_entry('surname', 'Didnt')
+field_entry('email', 'inn_did@gmail.com')
+field_entry('password', '12345qwert')
 
-password_field = driver.find_element_by_id('password')
-password_field.send_keys('12345qwert')
+# success_massage = driver.find_element(By.TAG_NAME, 'strong')
+# if 'Success' in success_massage.text:
+#     print('Test success_massage - PASSED')
+# else:
+#     print('Test success_massage - FAILED')
 
 time.sleep(3)
 driver.close()
